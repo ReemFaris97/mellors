2db71b52b0ede02eba7e3ff1df724d12e5f75572
AsmaaEsmail <asmaaesmail0@gmail.com>
2023-03-09 18:57:04 +0200

    change color dashboard

--- resources/views/admin/layout/styles.blade.php
+++ resources/views/admin/layout/styles.blade.php
@@ -1,8 +1,9 @@
-<link rel="shortcut icon" href="{{asset('_admin/assets/images/favicon.ico')}}">
+<link rel="shortcut icon" href="{{asset('_admin/assets/images/logo1.png')}}">
 <!--Morris Chart CSS -->
 <link rel="stylesheet" href="{{asset('_admin/assets/plugins/morris/morris.css')}}">
 
 <!-- App css -->
+<link href="{{asset('_admin/assets/fonts/fontawesome/css/all.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/css/bootstrap-rtl.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/css/core.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/css/components.css')}}" rel="stylesheet" type="text/css" />
@@ -11,7 +12,7 @@
 <link href="{{asset('_admin/assets/css/menu.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/css/responsive.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/summernote.css')}}" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
 
 <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -20,29 +21,31 @@
 <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 <![endif]-->
 <script src="{{asset('_admin/assets/js/modernizr.min.js')}}"></script>
-<link href="{{ asset('_admin/assets/plugins/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet"
-type="text/css" />
+<link href="{{ asset('_admin/assets/plugins/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" type="text/css" />
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Cairo:300,400,600,700&display=swap&subset=arabic" rel="stylesheet">
 
 <style>
-    body, html {
-        font-family: 'Cairo', sans-serif !important;
-        /*font-size: 16px;*/
-    }
+body,
+html {
+    font-family: 'Cairo', sans-serif !important;
+    /*font-size: 16px;*/
+}
 
-    .slimScrollBar {
-        display: block !important;
-        background: #ffffff !important;
-        width: 7px !important;
-    }
-    .toast-error, .toast-success {
-        opacity: unset !important;
-    }
+.slimScrollBar {
+    display: block !important;
+    background: #ffffff !important;
+    width: 7px !important;
+}
+
+.toast-error,
+.toast-success {
+    opacity: unset !important;
+}
 </style>
 <link href="{{asset('_admin/assets/css/customDashboard.css')}}" rel="stylesheet" type="text/css">
-<link href="{{asset('_admin/assets/plugins/multiselect/css/multi-select.css')}}" rel="stylesheet" type="text/css"/>
+<link href="{{asset('_admin/assets/plugins/multiselect/css/multi-select.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('_admin/assets/plugins/select2/dist/css/select2.css')}}" rel="stylesheet" type="text/css">
 <link href="{{asset('_admin/assets/plugins/select2/dist/css/select2-bootstrap.css')}}" rel="stylesheet" type="text/css">
