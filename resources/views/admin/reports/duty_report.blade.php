4f0cda610c75d1b8e753eebf05cd017c9652eaf7
AsmaaEsmail <asmaaesmail0@gmail.com>
2023-04-10 04:35:59 +0200

    fonts stoppage

--- resources/views/admin/reports/duty_report.blade.php
+++ resources/views/admin/reports/duty_report.blade.php
@@ -1,65 +1,71 @@
 @extends('admin.layout.app')
 @section('title')
-Duty Report 
+Duty Report
 @endsection
 @section('content')
 <div class="card-box ">
     <ul class="nav nav-tabs tabsLinkDuty">
-        <li class="{{ (request()->is('search_ride_ops_reports*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Ride">Ride Ops</a></li>
-        <li class="{{ (request()->is('search_health_and_safety*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Health">Health & safety </a></li>
-        <li class="{{ (request()->is('search_maintenance_reports*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Maintenance"> Maintenance</a></li>
-        <li class="{{ (request()->is('search_tech_reports*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Teachnical"> Teachnical Services</a></li>
-        <li class="{{ (request()->is('search_skill_game_reports*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Skill"> Skill Games</a></li>
-        <li class="{{ (request()->is('search_duty_summary_reports*')) ? 'active' : '' }}"><a data-toggle="tab" href="#Duty"> Duty Summary</a></li>
+        <li class="{{ (request()->is('search_ride_ops_reports*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Ride">Ride Ops</a></li>
+        <li class="{{ (request()->is('search_health_and_safety*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Health">Health & safety </a></li>
+        <li class="{{ (request()->is('search_maintenance_reports*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Maintenance"> Maintenance</a></li>
+        <li class="{{ (request()->is('search_tech_reports*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Teachnical"> Teachnical Services</a></li>
+        <li class="{{ (request()->is('search_skill_game_reports*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Skill"> Skill Games</a></li>
+        <li class="{{ (request()->is('search_duty_summary_reports*')) ? 'active' : '' }}"><a data-toggle="tab"
+                href="#Duty"> Duty Summary</a></li>
     </ul>
 
     <div class="tab-content tabsContentDuty">
         <div id="Ride" class="tab-pane fade in active">
-            <form action="{{url('/search_ride_ops_reports')}}" method="GET">
-            @csrf
-            @include('admin.reports.form')
-            </form>           
+            <form class="formSection" action="{{url('/search_ride_ops_reports')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+            </form>
             @include('admin.reports.show')
         </div>
 
         <div id="Health" class="tab-pane fade">
-        <form action="{{url('/search_health_and_safety')}}" method="GET">
-            @csrf
-            @include('admin.reports.form')
-            {!!Form::close() !!}
-            @include('admin.reports.show')
+            <form class="formSection" action="{{url('/search_health_and_safety')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+                {!!Form::close() !!}
+                @include('admin.reports.show')
         </div>
 
         <div id="Maintenance" class="tab-pane fade">
-        <form action="{{url('/search_maintenance_reports')}}" method="GET">
-            @csrf
-            @include('admin.reports.form')
-            {!!Form::close() !!}
-            @include('admin.reports.show')
+            <form class="formSection" action="{{url('/search_maintenance_reports')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+                {!!Form::close() !!}
+                @include('admin.reports.show')
         </div>
 
         <div id="Teachnical" class="tab-pane fade">
-        <form action="{{url('/search_tech_reports')}}" method="GET">
-            @csrf 
-            @include('admin.reports.form')
-            {!!Form::close() !!}
-            @include('admin.reports.show')
+            <form class="formSection" action="{{url('/search_tech_reports')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+                {!!Form::close() !!}
+                @include('admin.reports.show')
         </div>
 
         <div id="Skill" class="tab-pane fade">
-        <form action="{{url('/search_skill_game_reports')}}" method="GET">
-            @csrf
-            @include('admin.reports.form')
-            {!!Form::close() !!}
-            @include('admin.reports.show')
+            <form class="formSection" action="{{url('/search_skill_game_reports')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+                {!!Form::close() !!}
+                @include('admin.reports.show')
         </div>
 
         <div id="Duty" class="tab-pane fade">
-        <form action="{{url('/search_duty_summary_reports')}}" method="GET">
-            @csrf
-            @include('admin.reports.form')
-            {!!Form::close() !!}
-            @include('admin.reports.summery')
+            <form class="formSection" action="{{url('/search_duty_summary_reports')}}" method="GET">
+                @csrf
+                @include('admin.reports.form')
+                {!!Form::close() !!}
+                @include('admin.reports.summery')
         </div>
     </div>
 </div>
--- resources/views/admin/reports/ride_status.blade.php
+++ resources/views/admin/reports/ride_status.blade.php
@@ -1,18 +1,18 @@
 @extends('admin.layout.app')
 
 @section('title')
-    Rides Status
+Rides Status
 @endsection
 
 @section('content')
 
-    <div class="card-box">
+<div class="card-box">
 
-        <div id="datatable-buttons_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
-            <div class="row">
-                <div class="col-sm-12">
-                    <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
-                        <thead>
+    <div id="datatable-buttons_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer  ">
+        <div class="row">
+            <div class="col-sm-12">
+                <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap">
+                    <thead>
                         <tr role="row">
                             <th class="sorting_asc" tabindex="0" aria-controls="datatable-buttons" rowspan="1"
                                 colspan="1" aria-sort="ascending">ID
@@ -34,37 +34,35 @@
                                 rideSroppageDescription
                             </th>
                         </tr>
-                        </thead>
+                    </thead>
 
-                        <tbody>
+                    <tbody>
 
                         @foreach ($rides as $item)
-                            <tr role="row" class="odd" id="row-{{ $item->id }}">
-                                <td tabindex="0" class="sorting_1">{{ $item->id }}</td>
-                                <td>{{ $item->name }}</td>
-                                <td>{{ $item->parkName }}</td>
-                                <td> {{$item->available ?? ""}}</td>
-                                <td> {{$item->ride_notes}}</td>
-                                <td> {{$item->rideSroppageDescription}}</td>
-                            </tr>
+                        <tr role="row" class="odd" id="row-{{ $item->id }}">
+                            <td tabindex="0" class="sorting_1">{{ $item->id }}</td>
+                            <td>{{ $item->name }}</td>
+                            <td>{{ $item->parkName }}</td>
+                            <td> {{$item->available ?? ""}}</td>
+                            <td> {{$item->ride_notes}}</td>
+                            <td> {{$item->rideSroppageDescription}}</td>
+                        </tr>
 
                         @endforeach
 
-                        </tbody>
-                    </table>
+                    </tbody>
+                </table>
 
 
-                </div>
             </div>
-
         </div>
+
     </div>
+</div>
 
 
 @endsection
 
 @section('footer')
-    @include('admin.datatable.scripts')
+@include('admin.datatable.scripts')
 @endsection
-
-
