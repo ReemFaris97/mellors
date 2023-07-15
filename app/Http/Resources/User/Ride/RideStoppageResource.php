59613b19552abbe6eea43e6bf581ed52d0399715
nada <nadamedhatelsayed3@gmail.com>
2023-07-15 15:52:33 +0300

    pull

--- app/Http/Resources/User/Ride/RideStoppageResource.php
+++ app/Http/Resources/User/Ride/RideStoppageResource.php
@@ -20,10 +20,12 @@ class RideStoppageResource extends JsonResource
             'id' => $this->id,
             'ride_status' => $this->ride_status,
             'stopage_sub_category_id' => $this->stopage_sub_category_id,
+            'stopage_sub_category_name' => $this->stopageSubCategory?->name,
             'date' => $this->date,
             'opened_date' => $this->opened_date,
             'type' => $this->type,
             'description' => $this->description,
+            'ride_notes' => $this->ride_notes,
             'time_slot_start' => $this->time_slot_start,
             'stopage_category_id' => $this->stopage_category_id,
 
